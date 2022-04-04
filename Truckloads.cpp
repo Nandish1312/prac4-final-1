a1319155f0abebe941b40285c988c34fad619dad
Nandish1312 <76993210+Nandish1312@users.noreply.github.com>
2022-04-04 22:13:18 +0930

    Add files via upload

---
+++ Truckloads.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "Truckloads.h"
+
+Truckloads::Truckloads()
+{}
+
+int Truckloads::numTrucks(int numCrates,int loadSize)
+{
+    if(numCrates <= loadSize)
+    {
+        return 1;
+    }
+    else
+    {
+        return numTrucks((numCrates/2),loadSize)+numTrucks((numCrates-(numCrates/2)),loadSize);
+    }
+}
---
+++ Truckloads.h
@@ -0,0 +1,15 @@
+#ifndef TRUCKLOADS_H
+#define TRUCKLOADS_H
+#include <iostream>
+#include <string>
+
+class Truckloads
+{
+
+public:
+    Truckloads();
+    int numTrucks(int numCrates,int loadSize);
+
+};
+
+#endif
